280dbfef674106139137a1fde2bccb604bf9982c
perfectacle <perfectacle@naver.com>
2017-12-05 02:08:09 +0900

    add fail test

--- src/test/java/com/github/perfectacle/ci/SumTest.java
+++ src/test/java/com/github/perfectacle/ci/SumTest.java
@@ -11,4 +11,9 @@ public class SumTest {
     public void testAdd() {
         assertThat(Sum.add(1, 2), is(3));
     }
+
+    @Test
+    public void testAdd2() {
+        assertThat(Sum.add(1, 2), is(4));
+    }
 }
